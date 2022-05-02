7ab2f30df93cbbc678e9c5e4614050718d4d341d
“Ezgi <okurezgi@windowslive.com>
2022-05-02 19:09:54 +0300

    Done with phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+ return [arr[0],arr[1]]
+}
+
+const returnLastTwoDrivers = function(arr){
+    return [arr[arr.length-2],arr[arr.length-1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(number){
+    return function(fare){ return number*fare }
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}
+function selectDifferentDrivers(arr,funct){
+ return funct(arr)
+}
